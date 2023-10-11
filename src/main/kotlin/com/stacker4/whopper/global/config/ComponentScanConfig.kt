88f879b83e1e964e48e286b0ba98044453eb602f
JuuuuHong <juhang720@gmail.com>
2023-10-11 15:58:21 +0900

    add :: component scan config

--- src/main/kotlin/com/stacker4/whopper/global/config/ComponentScanConfig.kt
+++ src/main/kotlin/com/stacker4/whopper/global/config/ComponentScanConfig.kt
@@ -1,10 +1,10 @@
 package com.stacker4.whopper.global.config
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationPropertiesScan(
+@ComponentScan(
     basePackages = ["com.stacker4.whopper"]
 )
-class PropertiesScanConfig
+class ComponentScanConfig
