29591921dfcec386c0c458c188266d97ec39ee73
JuuuuHong <juhang720@gmail.com>
2023-10-11 15:57:03 +0900

    add :: properties scan config

---
+++ src/main/kotlin/com/stacker4/whopper/global/config/PropertiesScanConfig.kt
@@ -0,0 +1,12 @@
+package com.stacker4.whopper.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationPropertiesScan(
+    basePackages = [
+        "com.stacker4.whopper"
+    ]
+)
+class PropertiesScanConfig
