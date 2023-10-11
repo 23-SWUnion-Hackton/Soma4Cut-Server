f8386657ac4019807bcd51ca805439374581c254
JuuuuHong <juhang720@gmail.com>
2023-10-11 15:12:37 +0900

    add :: user entity

---
+++ src/main/kotlin/com/stacker4/whopper/domain/user/User.kt
@@ -0,0 +1,14 @@
+package com.stacker4.whopper.domain.user
+
+import javax.persistence.*
+
+@Entity
+class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+
+    @Column(nullable = false, unique = true)
+    val password: String
+)
