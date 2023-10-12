fbb65c3f575378d947fb65cd48e0d194fc390249
JuuuuHong <juhang720@gmail.com>
2023-10-12 20:29:54 +0900

    update :: query all code service

--- src/main/kotlin/com/stacker4/whopper/domain/code/repository/CodeRepository.kt
+++ src/main/kotlin/com/stacker4/whopper/domain/code/repository/CodeRepository.kt
@@ -1,7 +1,9 @@
 package com.stacker4.whopper.domain.code.repository
 
 import com.stacker4.whopper.domain.code.Code
+import com.stacker4.whopper.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CodeRepository : JpaRepository<Code, Long> {
+    fun findAllByUser(user: User): List<Code>
 }
--- src/main/kotlin/com/stacker4/whopper/domain/code/service/QueryAllCodeService.kt
+++ src/main/kotlin/com/stacker4/whopper/domain/code/service/QueryAllCodeService.kt
@@ -1,20 +1,29 @@
 package com.stacker4.whopper.domain.code.service
 
+import com.stacker4.whopper.common.security.SecurityUtil
 import com.stacker4.whopper.domain.code.presentation.data.response.QueryAllCodeResponse
 import com.stacker4.whopper.domain.code.repository.CodeRepository
+import com.stacker4.whopper.domain.user.exception.UserNotFoundException
+import com.stacker4.whopper.domain.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true)
 class QueryAllCodeService(
-    private val codeRepository: CodeRepository
+    private val codeRepository: CodeRepository,
+    private val securityUtil: SecurityUtil,
+    private val userRepository: UserRepository
 ) {
-    fun execute(): List<QueryAllCodeResponse> =
-        codeRepository.findAll().map {
+    fun execute(): List<QueryAllCodeResponse> {
+        val user = userRepository.findByIdOrNull(securityUtil.getCurrentUserId()) ?: throw UserNotFoundException()
+
+        return codeRepository.findAllByUser(user).map {
             QueryAllCodeResponse(
                 code = it.name,
                 createdAt = it.createdAt
             )
         }
+    }
 }
